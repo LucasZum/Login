ea528ab285b00e852aa704e42bf129a4bda3066b
Lucas Queiroz Jesus <96587878+LucasZum@users.noreply.github.com>
2022-05-31 11:00:52 -0300

    Atualização de sistema
    Adicionado o valor do dólar 
    Adicionado animação

--- real-time-conversor.js
+++ real-time-conversor.js
@@ -14,9 +14,49 @@ function conversor() {
     }).then((data)=>{
     
         let rate = data.USDBRL.high
+        let resultado = `R$ ${(rate * valor).toFixed(2)}`
 
-        let resultado = `R$ ${Math.round(rate * valor)}`
+
         document.getElementById("res").innerHTML = resultado
+
+        res.classList.toggle("aparecer")
+ 
+
+        
     
     })
 }
+
+
+
+function valor_dolar() {
+
+    let h4 = document.getElementById("valor_dolar_h4")
+
+    fetch(url).then((res)=>{
+
+        return res.json()
+    
+    }).then((data)=>{
+
+        let rate = data.USDBRL.high
+        let valor_dolar_h4 = `DÓLAR:  ${(rate * 1).toFixed(2)}`
+        h4.innerHTML = valor_dolar_h4
+
+    })
+}
+
+
+
+
+btn.addEventListener("click" , () => {
+    card.classList.toggle("transition")
+
+})
+btn.addEventListener("click" , () => {
+
+    btnVoltar.classList.toggle("mostrar")
+})
+btnVoltar.addEventListener("click", ()=>{
+    card.classList.toggle("transition")
+})
